8527abf03f294c1304844b15cd9db0c212d8197f
alexeirazinkov <a.ll.1@yandex.ru>
2022-01-26 17:28:44 +0300

    + deleting empty

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // формирует массив из строк, длинна которых меньше либо равна 3 символа.
 
 string[] array = {"1df","dd","3fdd","@"};
-string[] newarray = new string[5];
+string[] newarray = new string[30];
 // вводим счетчик
 int counter = 0;
 int counter1 = 0;
@@ -16,12 +16,13 @@ while (counter < array.Length)
     newarray[counter1] = array[counter];
     counter++;
     counter1++;
-
 }
+// удаляем пусте строки
+newarray = newarray.Where(x => x != null).ToArray();
 
 // вводим на экран новый массив
 while (newaraylength < newarray.Length)
 {
-    Console.WriteLine(newarray[newaraylength] + " ");
+    Console.WriteLine(newarray[newaraylength]);
     newaraylength++;
 }
