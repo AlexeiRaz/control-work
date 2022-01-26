35dcb82a50cef350a97fac1af8a8269e958cb323
alexeirazinkov <a.ll.1@yandex.ru>
2022-01-26 16:45:35 +0300

    + While

--- Program.cs
+++ Program.cs
@@ -2,14 +2,26 @@
 //  написать программу, которая из имеющегося массива строк
 // формирует массив из строк, длинна которых меньше либо равна 3 символа.
 
-string[] array = {"1df","2fdf","3fdfd","@"};
-string[] newarray = new string[10];
+string[] array = {"1df","dd","3fdd","@"};
+string[] newarray = new string[5];
 // вводим счетчик
+int counter = 0;
 int counter1 = 0;
-while (array[counter1].Length < 4)
+int newaraylength = 0;
+
+// записваем цикл
+while (counter < array.Length)
 {
-    newarray[counter1]= array[counter1];
+    if (array[counter].Length < 4)
+    newarray[counter1] = array[counter];
+    counter++;
     counter1++;
+
 }
 
-Console.WriteLine(newarray[counter1-1]);
+// вводим на экран новый массив
+while (newaraylength < newarray.Length)
+{
+    Console.WriteLine(newarray[newaraylength] + " ");
+    newaraylength++;
+}
