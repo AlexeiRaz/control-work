daba286bdde251356f9f3edb1165a94160eff8ad
alexeirazinkov <a.ll.1@yandex.ru>
2022-01-26 15:20:29 +0300

    +counter

--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@
 // формирует массив из строк, длинна которых меньше либо равна 3 символа.
 
 string[] array = {"1df","2fdf","3fdfd","@"};
+string[] newarray = new string[10];
+// вводим счетчик
+int counter1 = 0;
+while (array[counter1].Length < 4)
+{
+    newarray[counter1]= array[counter1];
+    counter1++;
+}
 
-int x = array[0].Length;
-Console.WriteLine(x);
+Console.WriteLine(newarray[counter1-1]);
